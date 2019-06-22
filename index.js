d820df2a13023870d62ab761e766547d82ab5a2b
mihalichpalich <pavlovtfspb@gmail.com>
2019-06-23 02:18:33 +0300

    отслеживание событий

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
-var things = require("./things");
-console.log(things.some_value);
-console.log(things.array_counter([1, 2, 3, 25, 64, 158]));
-console.log(things.multiply(5, 8));
+var events = require("events"); //модуль событий
+var util = require("util"); //модуль наследования
+
+// var myEmit = new events.EventEmitter(); //включение обработки событий
+
+// myEmit.on('some_event', function(text) {
+//   console.log(text);
+// });
+
+// myEmit.emit('some_event', 'Обработчик событий сработал!');
+
+var Cars = function(model) {
+  this.model=model;
+};
+
+util.inherits(Cars, events.EventEmitter); //наследование обработчика событий для всех Cars
+
+var bmw = new Cars('BMW');
+var audi = new Cars('Audi');
+var volvo = new Cars('Volvo');
+
+var cars = [bmw, audi, volvo];
+cars.forEach(function(car) {
+  car.on('speed', function(text) {
+  	console.log(car.model + " speed is - " + text);
+  });
+});
+
+bmw.emit('speed', '254.5 km');
+volvo.emit('speed', '180 km');

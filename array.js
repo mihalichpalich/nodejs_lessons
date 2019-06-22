133df34de5b8c90ce98a0bbc7d397c7b3ef5cd37
mihalichpalich <pavlovtfspb@gmail.com>
2019-06-23 01:47:46 +0300

    Множественный вывод из модуля

--- array.js
+++
@@ -1,5 +0,0 @@
-var array_counter = function(array) {
-  return "В массиве будет находиться " + array.length + " элемента!";
-};
-
-module.exports = array_counter;
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-var counter = require("./array");
-
-console.log(counter([1, 2, 3, 25, 64, 158]));
+var things = require("./things");
+console.log(things.some_value);
+console.log(things.array_counter([1, 2, 3, 25, 64, 158]));
+console.log(things.multiply(5, 8));

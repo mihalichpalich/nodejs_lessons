fae2772c902301fcac6786589b562f2cd12a35c1
mihalichpalich <pavlovtfspb@gmail.com>
2019-06-25 21:50:29 +0300

    изучил работу с файлами

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 var fs = require('fs'); //модуль для работы с файлами
 
-var file_readed = fs.readFileSync('text.txt', 'utf8'); //синхронное чтение текста
-console.log(file_readed);
+fs.readFile('text.txt', 'utf8', function(err, data) {
+  console.log(data);
+}); //асинхронное чтение файла, 3-й аргумент - ф-я, которая выполнится после завершения чтения (1-ый аргумент - ошибка, 
+//если файл не прочитан, 2-й - данные 'text.txt')
+
+fs.writeFile('some.txt', 'Hi, it is me', function(err, data) { });
+
+console.log("test"); //выведется раньше чем данные из файла
+
+// var message = "Привет, мир!\n" + file_readed;
+
+// fs.writeFileSync('some_new_file.txt', message);
+//console.log(file_readed);

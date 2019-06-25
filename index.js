56e722b86aebab9ea2d2736b5e29d04d283fb3ff
mihalichpalich <pavlovtfspb@gmail.com>
2019-06-25 20:30:35 +0300

    начал изучать работу с файлами

--- index.js
+++ index.js
@@ -1,30 +1,4 @@
-var events = require("events"); //модуль событий
-var util = require("util"); //модуль наследования
+var fs = require('fs'); //модуль для работы с файлами
 
-// var myEmit = new events.EventEmitter(); //включение обработки событий
-
-// myEmit.on('some_event', function(text) {
-//   console.log(text);
-// });
-
-// myEmit.emit('some_event', 'Обработчик событий сработал!');
-
-var Cars = function(model) {
-  this.model=model;
-};
-
-util.inherits(Cars, events.EventEmitter); //наследование обработчика событий для всех Cars
-
-var bmw = new Cars('BMW');
-var audi = new Cars('Audi');
-var volvo = new Cars('Volvo');
-
-var cars = [bmw, audi, volvo];
-cars.forEach(function(car) {
-  car.on('speed', function(text) {
-  	console.log(car.model + " speed is - " + text);
-  });
-});
-
-bmw.emit('speed', '254.5 km');
-volvo.emit('speed', '180 km');
+var file_readed = fs.readFileSync('text.txt', 'utf8'); //синхронное чтение текста
+console.log(file_readed);

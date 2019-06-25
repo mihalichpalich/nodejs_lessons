f964de89e413a51b66bbc2aef1b5ed07f4b9302a
mihalichpalich <pavlovtfspb@gmail.com>
2019-06-26 01:31:10 +0300

    Функция pipe(), работа с HTML и JSON

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 var fs = require('fs');
 
-var myReadShort = fs.createReadStream(__dirname + '/article.txt'); //метод для чтения потока, __dirname - константа для пути к папке
-var myWriteShort = fs.createWriteStream(__dirname + '/news.txt');
+//подключение к локальному серверу
+var http = require('http');
 
-myReadShort.on('data', function(chunk) {
-	console.log("Новые данные получены:\n");
-	myWriteShort.write(chunk);
-}); //обработчик для получения "кусочков" данных
+var server = http.createServer(function (req, res) {
+	console.log("URL страницы: " + req.url);
+	//res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+	res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+	// var myReadShort = fs.createReadStream(__dirname + '/index.html'); //метод для чтения потока, __dirname - константа для пути к папке
+	// myReadShort.pipe(res); //метод для вывода данных в файл потоками
+	var obj = {
+		model: "Audi",
+		speed: "234.5",
+		wheels: 4
+	};
+	res.end(JSON.stringify(obj)); //вывод объекта JSON в строку
+});
+
+server.listen(3000, '127.0.0.1');
+console.log("Мы отслеживаем порт 3000"); 

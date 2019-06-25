b5cb286ee4cb454c9559a168433d655fcccf02bb
mihalichpalich <pavlovtfspb@gmail.com>
2019-06-25 22:29:12 +0300

    изучил работу с директориями

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 var fs = require('fs'); //модуль для работы с файлами
 
-fs.readFile('text.txt', 'utf8', function(err, data) {
-  console.log(data);
-}); //асинхронное чтение файла, 3-й аргумент - ф-я, которая выполнится после завершения чтения (1-ый аргумент - ошибка, 
-//если файл не прочитан, 2-й - данные 'text.txt')
+//fs.unlink('text.txt', function () {}); //метод для удаления файлов
+fs.unlink('./new-one/some_new.txt', function () {
+	fs.rmdir('new-one', function() {});
+}); //чтобы удалить папку с файлами нужно сначала удалить файлы в ней
 
-fs.writeFile('some.txt', 'Hi, it is me', function(err, data) { });
+// fs.mkdir('new-one', function() {
+// 	fs.writeFile('./new-one/some_new.txt', "Привет мир!", function() {
+// 		console.log("Все сработало!");
+// 	});
+// }); //метод для создания папки
 
-console.log("test"); //выведется раньше чем данные из файла
-
-// var message = "Привет, мир!\n" + file_readed;
-
-// fs.writeFileSync('some_new_file.txt', message);
-//console.log(file_readed);
+//fs.rmdirSync('new-one'); //метод для удаления папки

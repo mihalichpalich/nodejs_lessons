78a77ab1fd60fd2958f6f111dd0ce61fc47c68e5
mihalichpalich <pavlovtfspb@gmail.com>
2019-06-26 02:14:02 +0300

    маршрутизация

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ var http = require('http');
 
 var server = http.createServer(function (req, res) {
 	console.log("URL страницы: " + req.url);
-	//res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-	res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
-	// var myReadShort = fs.createReadStream(__dirname + '/index.html'); //метод для чтения потока, __dirname - константа для пути к папке
-	// myReadShort.pipe(res); //метод для вывода данных в файл потоками
-	var obj = {
-		model: "Audi",
-		speed: "234.5",
-		wheels: 4
-	};
-	res.end(JSON.stringify(obj)); //вывод объекта JSON в строку
+    if (req.url === '/index' | req.url === '/') {
+		res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+		fs.createReadStream(__dirname + '/index.html').pipe(res);
+	} else if (req.url === '/about') {
+		res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+		fs.createReadStream(__dirname + '/about.html').pipe(res);
+	} else {
+		res.writeHead(404, {'Content-Type': 'text/html; charset=utf-8'});
+		fs.createReadStream(__dirname + '/404.html').pipe(res);
+	}
 });
 
 server.listen(3000, '127.0.0.1');

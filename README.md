c8ec3a9614c9633acc907bfcb5bc5bff858eb212
mihalichpalich <pavlovtfspb@gmail.com>
2019-06-26 17:49:10 +0300

    Создание HTML-формы и получение данных

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 для установки зависимостей в консоли набрать npm install
+
 и еще npm install supervisor -g, потом supervisor ./, чтобы не рестартить нод.жс
--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 var express = require('express');
+var bodyParser = require('body-parser'); //обработчик отправленных данных
 
 var app = express(); //доступ к ф-ям библиотеки express через переменную
+var urlencodedParser = bodyParser.urlencoded({ extended: false }); //обработчик данных из post-запроса
+
 var obj = {title: "Новость", id: 4, paragraphs: ['Параграф', 'Обычный текст', 'Числа: 2, 4, 6', 99]};
 
 app.set('view engine', 'ejs'); //добавление шаблонизатора
 
-app.use('/public', express.static('public')); //подключение статических файлов (css, img и т.д.), указываем url и саму папку
+app.use('/public', express.static('public')); //подключение статических файлов (css, img и т.д.), 
+											//указываем url и саму папку
 
 app.get('/', function(req, res) {
 	res.render('index');  //отображение шаблона из папки views (обязательно!) в браузере и передача параметров
 });
+
+app.post('/', urlencodedParser, function(req, res) {
+	if (!req.body) return res.sendStatus(400); //если форма пустая, выводим ошибку 400
+	console.log(req.body);
+	res.render('index-success', {data: req.body});
+}); //включение обработчика после отправки формы
 
 app.get('/about', function(req, res) {
 	res.render('about');

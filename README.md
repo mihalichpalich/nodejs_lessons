a77e63a440cef3e8104df77cc62cf40bfd3afea3
mihalichpalich <pavlovtfspb@gmail.com>
2019-06-26 17:05:01 +0300

    Статические файлы и промежуточное ПО

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 для установки зависимостей в консоли набрать npm install
+и еще npm install supervisor -g, потом supervisor ./, чтобы не рестартить нод.жс
--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 var express = require('express');
 
 var app = express(); //доступ к ф-ям библиотеки express через переменную
+var obj = {title: "Новость", id: 4, paragraphs: ['Параграф', 'Обычный текст', 'Числа: 2, 4, 6', 99]};
 
 app.set('view engine', 'ejs'); //добавление шаблонизатора
 
+app.use('/public', express.static('public')); //подключение статических файлов (css, img и т.д.), указываем url и саму папку
+
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/index.html');
+	res.render('index');  //отображение шаблона из папки views (обязательно!) в браузере и передача параметров
 });
 
 app.get('/about', function(req, res) {
-	res.sendFile(__dirname + '/about.html');
+	res.render('about');
 });
 
 app.get('/news/:id', function(req, res) {
-	var obj = {title: "Новость", id: 4, paragraphs: ['Параграф', 'Обычный текст', 'Числа: 2, 4, 6', 99]};
-	res.render('news', {newsID: req.params.id, obj: obj});  //отображение шаблона из папки views (обязательно!) 
-															//в браузере и передача параметров
+	res.render('news', {newsID: req.params.id, obj: obj});  
 });
 
 app.listen(3000);
